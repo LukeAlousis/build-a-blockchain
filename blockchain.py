53484cd25eedfef92701bf8d0602814d0929360a
Luke Alousis <lucasalousis@outlook.com>
2018-05-06 10:32:30 -0400

    Add to the blockchain class, including proof of work algorithim

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,106 @@
+import hashlib
+import json
+
+from time import time
+from uuid import uuid4
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # creates a new block and adds it to the chain
-        pass
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_transaction(self):
-        # adds a new transaction to the list of transactions
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block in the blockchain
+
+
+        :param proof: <int> The proof given by the proof of work algorithm
+        :param previous_hash: (optional) <str> The hash of the previous block
+        :return: <dict> New block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        # Add new block to the end of the chain
+        self.chain.append(block)
+
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+
+
+        :param sender: <str> Address of the sender
+        :param recipient: <str> Address of the recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the block that holds this transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
-        # hashes a block
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+
+
+        :param block: <dict> Block
+        :return: <str> SHA-256 hash of Block
+        """
+        # We must make sure the Dictionary is Ordered or we will have inconsistant hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        # Returns the last block on the chain
-        pass
+        # Returns the dictionary of the last block on the chain
+        return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple proof of work Algorithim:
+        - Find a number p' such that hash(pp') contains leading 4 zeros, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeros?
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
